24cf5446e92bc8aaac857c473708d1aae80bf62f
Ari Seyhun <Ari Seyhun>
2016-07-31 16:37:57 +0930

    Update

--- scan-movie.go
+++ scan-movie.go
@@ -196,7 +196,7 @@ func WatchfreeTo(url string) (string, error) {
 
 // googleSearch searches a query to google.com and returns all
 // the website url's on the first page in a slice of string.
-func googleSearch(query string) (results []string, err error) {
+func googleSearch(query string, pages int) (results []string, err error) {
 	results = make([]string, 0, 10)
 	query = strings.Replace(query, " ", "%20", -1)
 	resp, err := http.Get("https://www.google.com.au/search?q="+query)
@@ -210,6 +210,36 @@ func googleSearch(query string) (results []string, err error) {
 	}
 
 	sites := strings.Split(string(body), `class="g"`)
+	for _, site := range sites {
+		if strings.Index(site, `<a href="`) != -1 {
+			site = site[strings.Index(site, `<a href="`) + len(`<a href="`):]
+			if site[:len(`/url?q=`)] == `/url?q=` {
+				if strings.Index(site, `">`) != -1 {
+					site = site[len(`/url?q=`):strings.Index(site, `">`)]
+					if strings.Index(site, `&`) != -1 {
+						site = site[:strings.Index(site, `&`)]
+						site, err := url.QueryUnescape(site)
+						if err != nil {
+							return results, err
+						}
+						results = append(results, site)
+					}
+				}
+			}
+		}
+	}
+
+	resp, err = http.Get("https://www.google.com.au/search?q="+query+"&start=10")
+	if err != nil {
+		return results, err
+	}
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return results, err
+	}
+
+	sites = strings.Split(string(body), `class="g"`)
 	for _, site := range sites {
 		if strings.Index(site, `<a href="`) != -1 {
 			site = site[strings.Index(site, `<a href="`) + len(`<a href="`):]
